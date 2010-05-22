435401ca6525104789f53caa9a235ec936aeb9db
void <git.email.address@googlemail.com>
2010-05-22 21:06:20 +0200

    Moved the functions around a bit, added more error handling, got rid of the old redundant torrent unit

--- Torrent.rb
+++ Torrent.rb
@@ -10,11 +10,13 @@ class Torrent
 	
 	def processData
 		while @offset < input.size
-			
+			unit = readunit
+			@units << unit
 		end
 	end
 	
 	def nextByte
+		return nil if @offset >= @input.size
 		return @input[@offset]
 	end
 	
@@ -22,48 +24,67 @@ class Torrent
 		letter = nextByte
 		case letter
 		when 'i'
-			#integer: i<digits>e
-			@offset += 1
-			offset = @input.index('e', @offset)
-			raise 'Non-terminated integer' if offset == nil
-			numberString = @input[@offset..(offset - 1)]
-			raise "Invalid integer length: #{numberString}" if !numberString.isNumber
-			number = numberString.to_i
-			return number
+			return readInteger
 		when 'l'
-			output = []
-			while true
-				unit = readUnit
-				return output if unit == nil
-				output << unit
-			end
+			return readList
 		when 'd'
-			output = {}
-			while true
-				key = readUnit
-				return output if key == nil
-				value = readUnit
-				output[key] = value
-			end
+			return readDictionary
 		when 'e'
 			#terminator of dictionaries/lists
 			return nil
+		when nil
+			raise 'Incomplete list/dictionary'
 		else
 			if letter.isNumber
-				#<length digits>:<content of legth specified by the previous field>
-				offset = @input.index(':', @offset + 1)
-				raise 'Non-terminated string' if offset == nil
-				numberString = @input[@offset..(offset - 1)]
-				raise "Invalid string length: #{numberString}" if !numberString.isNumber
-				number = numberString.to_i
-				@offset = offset + 1
-				newOffset = @offset + number
-				string = @input[@offset..(newOffset - 1)]
-				@offset = newOffset
-				return string
+				return readString
 			else
 				raise "Invalid unit type: #{letter}"
 			end
 		end
+	end
+	
+	def readInteger
+		#integer: i<digits>e
+		@offset += 1
+		offset = @input.index('e', @offset)
+		raise 'Non-terminated integer' if offset == nil
+		numberString = @input[@offset..(offset - 1)]
+		raise "Invalid integer length: #{numberString}" if !numberString.isNumber
+		number = numberString.to_i
+		return number
+	end
+	
+	def readList
+		output = []
+		while true
+			unit = readUnit
+			return output if unit == nil
+			output << unit
+		end
+	end
+	
+	def readDictionary
+		output = {}
+		while true
+			key = readUnit
+			return output if key == nil
+			value = readUnit
+			output[key] = value
+		end
+	end
+	
+	def readString
+		#<length digits>:<content of legth specified by the previous field>
+		offset = @input.index(':', @offset + 1)
+		raise 'Non-terminated string' if offset == nil
+		numberString = @input[@offset..(offset - 1)]
+		raise "Invalid string length: #{numberString}" if !numberString.isNumber
+		number = numberString.to_i
+		@offset = offset + 1
+		newOffset = @offset + number
+		raise "Invalid string length: #{number}" if newOffset >= @input.size
+		string = @input[@offset..(newOffset - 1)]
+		@offset = newOffset
+		return string
 	end
 end
--- TorrentUnit.rb
+++
@@ -1,8 +0,0 @@
-class TorrentUnit
-	attr_reader :description, :data
-	
-	def initialize(description, data)
-		@description = description
-		@data = data
-	end
-end
